2381a12effa07eb3ed64d87b268ed6aec216177b
qhtkdrma <kimwonkyu15@gmail.com>
2023-10-05 18:47:08 +0900

    Initial commit

---
+++ src/main/java/com/traveler/travelcommunity/TravelcommunityApplication.java
@@ -0,0 +1,13 @@
+package com.traveler.travelcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelcommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelcommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/traveler/travelcommunity/TravelcommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traveler.travelcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelcommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
